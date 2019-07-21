e98f2dc268587357e981157c8353baba15d68a54
Marcus Trautwig <marcus@trautwig.de>
2019-07-21 14:25:08 +0200

    update Reveal.js to 3.8.0

--- functions.php
+++ functions.php
@@ -4,9 +4,9 @@ add_theme_support( 'post-thumbnails' );
 
 add_action( 'wp_enqueue_scripts', function() {
     wp_enqueue_style( 'style', get_stylesheet_uri() );
-    wp_enqueue_style( 'reveal', get_template_directory_uri() . '/reveal.js/css/reveal.min.css' );
+    wp_enqueue_style( 'reveal', get_template_directory_uri() . '/reveal.js/css/reveal.css' );
     wp_enqueue_style( 'reveal-theme-black', get_template_directory_uri() . '/reveal.js/css/theme/black.css' );
-    wp_enqueue_script( 'script', get_template_directory_uri() . '/reveal.js/js/reveal.min.js' );
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/reveal.js/js/reveal.js' );
 });
 
 add_action( 'customize_register', function($wp_customize) {
